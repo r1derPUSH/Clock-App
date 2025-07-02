30b6977df7284064997fe7cde5aed5cf8d162dd0
Maksym Kopak <kopakmaksym@icloud.com>
2025-07-02 13:51:02 +0300

    build fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/reactTest-17-2try/"
+  base: "/react-17-gitproj/"
 })
